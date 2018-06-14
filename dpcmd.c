eb6265f95f15ac24896e173c1e396d4471b1435a
root <root@localhost.localdomain>
2018-06-14 13:49:51 +0800

    modify lock command issue

--- dpcmd.c
+++ dpcmd.c
@@ -503,7 +503,7 @@ int main(int argc, char *argv[])
  
 	//signal(SIGINT, sin_handler);
 
-	printf("\nDpCmd Linux 1.8.1.%02d Engine Version:\nLast Built on May 25 2018\n\n",GetConfigVer()); //1. new feature.bug.config
+	printf("\nDpCmd Linux 1.9.1.%02d Engine Version:\nLast Built on May 25 2018\n\n",GetConfigVer()); //1. new feature.bug.config
 
 	g_ucOperation=0;
 	GetLogPath(g_LogPath);
