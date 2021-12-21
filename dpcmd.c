bf9a2f4bc730fe12a9ada0d7d60cf18f118887c4
root <root@fedora27.lab.local>
2021-12-21 17:51:07 +0800

    dpcmd.c

--- dpcmd.c
+++ dpcmd.c
@@ -1588,45 +1588,47 @@ void do_DisplayOrSave(void)
                 printf("\n\n");
             } else {
                 UploadAddrRange.length = UploadAddrRange.end - UploadAddrRange.start;
-                char SourceStr[64];   
+               
+		char SourceStr[64];   
 	 
 
 		sprintf(SourceStr, "%s", g_parameter_read);
 	 	
-		
-
-		int len = strlen(SourceStr);
-		int i = len;
-		
-		while(SourceStr[i]!='.' && i>0)
-		    i--;
-		
-		char ExtStr[64];
-		if(SourceStr[i]=='.')
+		if(dev_cnt>1)
 		{
-              	    sprintf(ExtStr, "%s", &SourceStr[i]); 
-		    char *loc = strstr(SourceStr,ExtStr); 
-			  
-		    for(int i=loc-SourceStr;i<strlen(SourceStr);i++) 
-	  	        SourceStr[i]='\0';
-		   // printf("\n 1ExtStr= %s",ExtStr);
-		    char str[64];
+			int len = strlen(SourceStr);
+			int i = len;
+			
+			while(SourceStr[i]!='.' && i>0)
+			    i--;
+			
+			char ExtStr[64];
+			if(SourceStr[i]=='.')
+			{
+		      	    sprintf(ExtStr, "%s", &SourceStr[i]); 
+			    char *loc = strstr(SourceStr,ExtStr); 
+				  
+			    for(int i=loc-SourceStr;i<strlen(SourceStr);i++) 
+		  	        SourceStr[i]='\0';
+			   // printf("\n 1ExtStr= %s",ExtStr);
+			    char str[64];
 
-		    sprintf(str, "_%d", icnt + 1);
-           	    strcat(SourceStr, str);
-		    strcat(SourceStr, ExtStr);
-		}
-		else
-		{
-              	    sprintf(ExtStr, "%s", &SourceStr[0]);
-		    //printf("\n 2ExtStr= %s",ExtStr);
+			    sprintf(str, "_%d", icnt + 1);
+		   	    strcat(SourceStr, str);
+			    strcat(SourceStr, ExtStr);
+			}
+			else
+			{
+		      	    sprintf(ExtStr, "%s", &SourceStr[0]);
+			    //printf("\n 2ExtStr= %s",ExtStr);
 
-		    char str[64];
-		    sprintf(str, "_%d", icnt + 1); 
-		    strcat(SourceStr, str); 
+			    char str[64];
+			    sprintf(str, "_%d", icnt + 1); 
+			    strcat(SourceStr, str); 
+			} 
 		}
- 
-		 
+
+			
                 if (WriteFile((const char*)SourceStr, pBufferForLastReadData[icnt], UploadAddrRange.length) == 1)
                     printf("\nSuccessfully saved into file:%s \n", SourceStr);
                 else
